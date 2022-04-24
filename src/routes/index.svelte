ef5329c29d255b2cffe5eb24ba93180f26558fff
josepeha <joseperazahays@gmail.com>
2022-04-24 14:37:48 -0600

    Feat[master]: created main page structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,11 @@
-<p>hello, friend</p>
+<script>
+    import TodoForm from '../components/TodoForm.svelte';
+</script>
+
+<main>
+    <h1 class="text-2xl font-bold text-center text-gray-800 md:text3xl">
+        My Todos
+    </h1>
+    <TodoForm />
+
+</main>
