ee98ad79df08cb1ea21853353ac1f8443537be54
josepeha <joseperazahays@gmail.com>
2022-04-24 14:38:41 -0600

    Feat[master]: created TodoForm with input field, label and button

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,20 @@
+<script>
+    let todo = '';
+    function handleSubmit() {
+        console.log(todo);
+    }
+</script>
+
+<form on:submit|preventDefault={handleSubmit} class="my-6">
+    <div class="flex flex-col text-sm mb-2">
+        <label for="todo">Todo</label>
+        <input bind:value={todo}
+               type="text" id="todo" name="todo"
+               placeholder="What needs doing?"
+               class="appearance-none shadow-smrounded-lg
+                      border-gray-200 p-2 focus:outline-none">
+    </div>
+    <button type="submit"
+            class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600"
+    >Add Todo</button>
+</form>
